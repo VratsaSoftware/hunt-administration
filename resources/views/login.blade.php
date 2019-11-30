49923eaf694d8f6a198f6a4785adcff50b0264d5
Tsvetosval_G <tsetsig@abv.bg>
2019-11-30 19:45:49 +0200

    Create login page
    Login page for our admin panel

---
+++ resources/views/login.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Hunt Admin Panel | Login</title>
+        <link rel="stylesheet" type="text/css" href="css/login.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body class="login-page">
+        <h1 class="form-title">Hunt Admin Panel</h1>
+        <form method="POST" action="#" class="login-form">
+            <input type="email" name="email" placeholder="Email">
+            <input type="password" name="password" placeholder="Password">
+            <input type="submit" name="login" value="Login">
+        </form>
+    </body>
+</html>
